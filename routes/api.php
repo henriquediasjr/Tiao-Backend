7372167e541070db4c158f7c384f6c2ea1f668ae
Henrique Dias de Carvalho Junior <henriquediasjr@gmail.com>
2025-01-09 14:51:18 -0300

    setting api routes on webphp file

--- routes/api.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\MusicaController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/musicas', [MusicaController::class, 'index']);
-Route::post('/musicas', [MusicaController::class, 'store']);
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
+use App\Http\Controllers\MusicaController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::middleware('api')->group(function () {
+//     Route::get('/top5', [MusicaController::class, 'index']);
+//     Route::post('/sugerir', [MusicaController::class, 'store']);
+// });
+
+Route::prefix('api')->middleware('api')->group(function () {
+    Route::get('/top5', [MusicaController::class, 'index']);
+    Route::post('/sugerir', [MusicaController::class, 'store']);
 });
