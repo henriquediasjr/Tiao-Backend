45fac9c4e82e182f61c45c13a96a5519f3f48438
Henrique Dias de Carvalho Junior <henriquediasjr@gmail.com>
2025-01-10 12:23:44 -0300

    linking eloquent model to the model factory

--- app/Models/Musica.php
+++ app/Models/Musica.php
@@ -2,9 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Musica extends Model
 {
+    use HasFactory;    
     protected $fillable = ['titulo', 'youtube_id', 'thumb', 'visualizacoes'];
 }
